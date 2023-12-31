664f5af98b40f4c098c6d88aebb11fad56d8d5a9
Phelyppe Álex <phelyppesilva@gmail.com>
2023-12-31 18:48:47 -0300

    correções

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+
+  const MyApp({
+    super.key
+  });
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,35 +19,44 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final String baseUrl = 'http://192.168.148.161';
+  TextEditingController controller = TextEditingController();
+  
+  MyHomePage({
+    super.key
+  });
+
+  final String baseUrl = 'http://192.168.100.108';
 
   void sendCommand(String route) async {
     String uri = '$baseUrl$route';
-    final response = await http.get(Uri.parse(uri));
+    await http.get(Uri.parse(uri));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Automação Residencial'),
+        title: const Text('Automação Residencial'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const TextField(
+              
+            ),
             ElevatedButton(
               onPressed: () {
                 sendCommand('/ligar'); // Enviar comando para ligar o LED
               },
-              child: Text('Ligar luz'),
+              child: const Text('Ligar luz'),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 sendCommand('/desligar'); // Enviar comando para desligar o LED
               },
-              child: Text('Apagar luz'),
+              child: const Text('Apagar luz'),
             ),
           ],
         ),
